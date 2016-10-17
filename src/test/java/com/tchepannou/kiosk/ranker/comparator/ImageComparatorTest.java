b661e798a695d51d1810353953ac814ef899d179
htchepannou <htchepannou@expedia.com>
2016-10-17 00:03:28 -0400

    improve coverage

--- src/test/java/com/tchepannou/kiosk/ranker/comparator/ImageComparatorTest.java
+++ src/test/java/com/tchepannou/kiosk/ranker/comparator/ImageComparatorTest.java
@@ -23,6 +23,49 @@ public class ImageComparatorTest {
 
     }
 
+    @Test
+    public void testCompare2() throws Exception {
+        // Given
+        final Rankable r1 = createRankable(false);
+        final Rankable r2 = createRankable(true);
+
+        // when
+        final int result = new ImageComparator().compare(r1, r2);
+
+        // Then
+        assertThat(result).isEqualTo(-1);
+
+    }
+
+    @Test
+    public void testCompare2Images() throws Exception {
+        // Given
+        final Rankable r1 = createRankable(true);
+        final Rankable r2 = createRankable(true);
+
+        // when
+        final int result = new ImageComparator().compare(r1, r2);
+
+        // Then
+        assertThat(result).isEqualTo(0);
+
+    }
+
+    @Test
+    public void testCompare0Image() throws Exception {
+        // Given
+        final Rankable r1 = createRankable(false);
+        final Rankable r2 = createRankable(false);
+
+        // when
+        final int result = new ImageComparator().compare(r1, r2);
+
+        // Then
+        assertThat(result).isEqualTo(0);
+
+    }
+
+
     private Rankable createRankable(final boolean withImage){
         final Rankable r = mock(Rankable.class);
         when(r.isWithImage()).thenReturn(withImage);
